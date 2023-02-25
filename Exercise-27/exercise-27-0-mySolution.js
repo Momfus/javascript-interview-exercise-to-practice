659c7ad761cf96c6023ebc228d402833c839f1a0
Momfus <momfus@outlook.com>
2023-02-25 04:13:50 -0300

    Exercise 27 done

--- Exercise-27/exercise-27-0-mySolution.js
+++ Exercise-27/exercise-27-0-mySolution.js
@@ -1,12 +1,29 @@
 /*
 # Enunciado Ejercicio 27:
-
+   Dado un número, mostra a cuantos años, meses y días equivalen.
    
 #Ejemplos:
    
+   calcularDias(920);
+
    Retorna:
+      Equivale a 2 años, 6 meses y 10 días.
       
 # Sugerencias de cómo hacerlo: 
-   - C
+   - Crear una función con parámetros días.
+   - Divisiones para sacar años, meses y días.
 
 */
+
+function calculateDays( totalDias ) {
+
+   let anios = Math.floor(totalDias / 365);
+
+   let calcAux = (totalDias % 365); 
+   let meses = Math.floor(calcAux / 30);
+
+   let dias = calcAux % 30;
+   return `Equivale a ${anios} años, ${meses} meses y ${dias} días`;
+}
+
+console.log( calcularDias(920) );
--- Exercise-27/exercise-27-1-anotherSolution.js
+++ Exercise-27/exercise-27-1-anotherSolution.js
@@ -1,2 +1,14 @@
 // Resolución realizada por quien hizo el ejercicio:#
 
+function calcularDias(dias) {
+
+   let anios = Math.floor(dias / 365);
+   let dias_restantes = (dias % 365);
+   let meses = Math.floor(dias_restantes / 30);
+
+   dias_restantes = dias_restantes % 30;
+
+   return `Equivale a ${anios} años, ${meses} meses y ${dias_restantes} días.`;
+}
+
+console.log( calcularDias(920) );
