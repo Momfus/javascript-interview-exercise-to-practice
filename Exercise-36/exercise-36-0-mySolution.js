de6f8d97da10d24b8c00a342e34c561675af01f6
Momfus <momfus@outlook.com>
2023-02-28 02:36:45 -0300

    Exercise 36 done

---
+++ Exercise-36/exercise-36-0-mySolution.js
@@ -0,0 +1,50 @@
+/*
+# Enunciado Ejercicio 36:
+
+   
+#Ejemplos:
+
+   Dado un texto, devolver cuantas consonantes y cuantas vocales tiene.
+   
+   Ejemplo: 
+      countLetters('momfusArboleo.com');
+
+      Return:
+         Consonantes: 10
+         Vocales: 7
+
+
+# Sugerencias de cómo hacerlo: 
+   - Función con parámetro texto
+   - Recorrer cada letra
+   - Testear con expresión regular.
+
+*/
+
+function countLetters( text ) {
+
+   let cantidadVocales = 0,
+       cantidadConsonantes = 0;
+
+   // First separte as array, then only use the letters (no special characters) and then search for vowels
+   let textArrayChar = text.split('').filter( char => ( isNaN(char) && (/[\w]/gi).test(char) ) );
+
+   textArrayChar.forEach( letter => {
+      
+      if( (/^[aeiou]$/i).test(letter) ) {
+         cantidadVocales++;
+      } else {
+         cantidadConsonantes++;
+      }
+      
+   });
+   
+   console.log(`
+   Consonantes: ${cantidadConsonantes}
+   Vocales: ${cantidadVocales}
+   `
+   );
+
+}
+
+countLetters('momfusArboleo1234.com');
---
+++ Exercise-36/exercise-36-1-anotherSolution.js
@@ -0,0 +1,30 @@
+
+function cuentaLetras( texto ) {
+
+   let   consonantes = 0,
+         vocales = 0, 
+         texto_limpio = '';
+
+   // Toma en cuenta las vocales con tiltes
+   texto_limpio = texto.split('')
+                        .filter( letra => (/[áéíóú\w]/gi).test(letra) && isNaN(letra))
+                        .join('');
+
+   for( let letra of texto_limpio ) {
+
+      if( /[áéíóíaeiou]/gi.test(letra) ) {
+         vocales++;
+      } else {
+         consonantes++;
+      }
+
+   }
+
+   return [consonantes, vocales];
+
+}
+
+let letras = cuentaLetras('hol!! . , - que tal á ó');
+
+console.log("Consonantes: ", letras[0]);
+console.log("Vocales: ", letras[1]);
